b7f4b542982a93bfbdf147f41eca18a0b8a7aaa1
jmguevarra <jaimeguevarra22@gmail.com>
2021-07-06 18:38:51 +0800

    fixed responsiveness

--- js/main.js
+++ js/main.js
@@ -13,7 +13,16 @@ const checkDeviceLocation = function(){
                return res.json();  //shor with Es6 res => res.json()
             }).then(function(data){
                 //console.log(data);
-                displayDeviceLocation.textContent = `${data.locality} ${data.city}, ${data.postcode} ${data.principalSubdivision}, ${data.countryName}`;
+                let htmlRender = `<div class="location-details">
+                <span class="country"><b>Country:</b> ${data.countryName}</span>
+                <span class="region"><b>Region:</b>  ${data.principalSubdivision}</span>
+                <span class="postalcode"><b>Postal Code:</b>  ${data.postcode}</span>
+                <span class="city"><b>City:</b>  ${data.city}</span>
+                <span class="locality"><b>Locality:</b>  ${data.locality}</span>
+                </div>`;
+
+                displayDeviceLocation.innerHTML = htmlRender;
+                //displayDeviceLocation.textContent = `${data.locality} ${data.city}, ${data.postcode} ${data.principalSubdivision}, ${data.countryName}`;
             });
     };
 
@@ -31,8 +40,24 @@ const findLocationBtn = document.querySelector('#check-location');
 findLocationBtn.addEventListener('click', function(){
     checkDeviceLocation();
 
-    setTimeout(function(){ //remove after 3sec
-        const displayDeviceLocation = document.querySelector('.display-location')
-        displayDeviceLocation.textContent = '';
-    }, 3000);
+    // setTimeout(function(){ //remove after 3sec
+    //     const displayDeviceLocation = document.querySelector('.display-location')
+    //     displayDeviceLocation.innerHTML = '';
+    // }, 5000);
+});
+
+
+
+//Menu Burger
+const menuButton = document.querySelector('.menu-button');
+menuButton.addEventListener('click', function(){
+    const menu = document.querySelector('.menu');
+
+    if(this.classList.contains('toggled')){
+        this.classList.remove('toggled');
+        menu.classList.remove('menu-toggled');
+    }else{
+        this.classList.add('toggled'); 
+        menu.classList.add('menu-toggled');
+    }
 });
